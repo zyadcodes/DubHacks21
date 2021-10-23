b7b2b4e56410d2824a8bf17b2e5ac2a89325cb76
Haashim Ameer <34925045+HaashimAmeer@users.noreply.github.com>
2021-10-23 15:41:26 -0700

    Update restrictions.js

---
+++ config/restrictions.js
@@ -0,0 +1,53 @@
+//restrictions specific to each
+const lifestyles = {
+  halal: ["pork", "wine", "alcohol", "gelatin", "whey"],
+  kosher: [
+    "Shellfish",
+    "Shrimp",
+    "Prawns",
+    "Lobster",
+    "Scallops",
+    "Mussels",
+    "Oysters",
+    "Squid",
+    "Octopus",
+    "Crabs",
+  ],
+  vegetarian: ["Egg", "Dairy", "Milk", "Cheese", "Honey"],
+  vegan: [],
+};
+
+//list of all ingredients
+const allRestrictions = [
+  "Walnuts",
+  "Lactose",
+  "Peanuts",
+  "Eggs",
+  "Gluten",
+  "Shellfish",
+  "Shrimps",
+  "Prawn",
+  "Squid",
+  "Crab",
+  "Lobster",
+  "Soy",
+  "Wheats",
+  "Peanuts",
+  "Cashews",
+  "Almonds",
+  "Hazelnuts",
+  "Tree Nuts",
+  "Lupin",
+  "Land snails",
+  "Celery",
+  "Leaves",
+  "Seeds",
+  "Stalks",
+  "Sesame",
+  "Lecithin",
+  "Soy milk",
+  "Citrus",
+  "Fish",
+];
+
+export { lifestyles, allRestrictions };
